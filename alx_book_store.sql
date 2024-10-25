fb75053cdc3e93f58a7beadff37e5ee68049bdb4
Agent1c <medicebyb@gmail.com>
2024-10-25 07:27:21 +0200

    0. A Magical Database for Your Dream Online Bookstore Adventure!

---
+++ alx_book_store.sql
@@ -0,0 +1,47 @@
+--Implementation of Database alx_book_store:
+CREATE DATABASE alx_book_store;
+
+USE alx_book_store;
+--Check for creation and implementation of Authors
+CREATE TABLE Authors(
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+--creation and implementation of Books Table
+
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY(author_id) REFERENCES Authors(author_id)
+);
+
+--Check for creation and implementation of Customers Table
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215) UNIQUE,
+    address TEXT
+);
+
+--Check for creation and implementation of Orders Table
+
+CREATE TABLE Orders(
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+);
+
+--Check for creation and implementation of Order_Details Table
+CREATE TABLE Order_Details(
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
